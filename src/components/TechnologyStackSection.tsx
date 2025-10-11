27a59febc555e05ee3d86f9d31308918bbc13ad6
[dyad] <ebenezersocials@gmail.com>
2025-10-11 20:18:18 +0200

    [dyad] Increased logo width and height - wrote 1 file(s)

--- src/components/TechnologyStackSection.tsx
+++ src/components/TechnologyStackSection.tsx
@@ -32,7 +32,7 @@ const TechnologyStackSection = () => {
               <img
                 src={tech.logo}
                 alt={tech.name}
-                className="h-48 w-auto object-contain"
+                className="h-48 w-48 object-contain"
               />
             </li>
           ))}
@@ -46,7 +46,7 @@ const TechnologyStackSection = () => {
               <img
                 src={tech.logo}
                 alt={tech.name}
-                className="h-48 w-auto object-contain"
+                className="h-48 w-48 object-contain"
               />
             </li>
           ))}
