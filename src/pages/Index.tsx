1a0c0dbc54c16426f597ce4ba8912cb077c78fb7
[dyad] <ebenezersocials@gmail.com>
2025-10-11 20:22:14 +0200

    [dyad] Added scrolling tech logo section - wrote 10 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,6 +7,7 @@ import SubBrandsSection from "@/components/SubBrandsSection";
 import AboutUsSection from "@/components/AboutUsSection";
 import ServicesSection from "@/components/ServicesSection";
 import SearchBar from "@/components/SearchBar";
+import TechnologyStackSection from "@/components/TechnologyStackSection";
 
 const Index = () => {
   return (
@@ -22,6 +23,7 @@ const Index = () => {
         <div className="container mx-auto px-4 py-16 max-w-screen-2xl space-y-16">
           <AboutUsSection />
           <ServicesSection />
+          <TechnologyStackSection />
           <SubBrandsSection />
         </div>
       </main>
